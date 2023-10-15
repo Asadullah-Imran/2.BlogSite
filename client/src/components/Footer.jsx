4cf3ea15f351f53bee9db9202a47938f928b0d4c
Asadullah-Imran <asadullahimran19@gmail.com>
2023-10-15 20:36:54 +0600

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src="./logo.png" alt="logo pic" />
+      <span>
+        Made with love and <b>React.js</b>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
