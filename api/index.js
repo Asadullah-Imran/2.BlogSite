fa87074614162d174afb85d1811e799a9f55580b
Asadullah-Imran <asadullahimran19@gmail.com>
2023-10-16 15:54:39 +0600

    2nd be

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { default as authRoutes, default as postRoutes, default as usersRoutes } from "./routes/posts.js";
 
 const app=express();
 
+app.use(express.json());
+
+app.use("/api/posts",postRoutes)
+app.use("/api/auth",authRoutes)
+app.use("/api/users",usersRoutes)
 
 
 
